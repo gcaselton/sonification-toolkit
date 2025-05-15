de49c992deef0e7ff92992bc34a6ad7b444ec0b3
George Caselton <ngc133@newcastle.ac.uk>
2025-05-15 17:02:33 +0100

    Initial commit - add quick sonify function and star_query module

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from star_query import sonify_star
+import lightkurve as lk
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# x = [0,1,2,3,4,5,6,7,8,9,20,50]
+# y = [2, 4, 19, 1, 11, 21, 15, 4, 6, 22, 20, 18]
+
+star_name = input('Enter name of star to sonify: ')
+
+sonify_star(star_name, 'K2')
+    
+
+
