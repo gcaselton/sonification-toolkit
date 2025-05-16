843b821607b8ca144f4e1378821abfe94f1c5599
George Caselton <ngc133@newcastle.ac.uk>
2025-05-16 12:52:43 +0100

    Began implementing ability to choose mission for light curves

--- src/main.py
+++ src/main.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 # x = [0,1,2,3,4,5,6,7,8,9,20,50]
 # y = [2, 4, 19, 1, 11, 21, 15, 4, 6, 22, 20, 18]
 
-star_name = input('Enter name of star to sonify: ')
+# star_name = input('Enter name of star to sonify: ')
 
-sonify_star(star_name, 'K2')
+star_name = 'Kepler-10'
+
+sonify_star(star_name)
     
 
 
