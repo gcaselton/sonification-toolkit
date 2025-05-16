b96b2d760a8ca88df11fe5c1d3edf1da29fc7120
George Caselton <ngc133@newcastle.ac.uk>
2025-05-16 12:52:43 +0100

    Began implementing ability to choose mission for light curves

--- src/star_query.py
+++ src/star_query.py
@@ -6,6 +6,8 @@ from extensions import sonify
 
 def filter_by_author(search_result):
 
+    # To do - determine which HLSPs/pipelines are the best for our purposes
+
     filtered_result = search_result[(search_result.author == "SPOC") | (search_result.author == "TESS-SPOC")]
 
     if len(filtered_result) > 0:
@@ -18,24 +20,48 @@ def filter_by_author(search_result):
         else:
             return search_result
         
-def search_for_star(star, mission):
+def search_for_star(star_name):
 
     # To do - check if star is present in multiple missions, then offer the user a choice
 
-    print('Searching the Universe for ' + star + '...')
+    print('Searching the Universe for ' + star_name + '...')
 
-    search_result = lk.search_lightcurve(star, mission=mission, exptime=120)
+    search_result = lk.search_lightcurve(star_name)
 
     if len(search_result) == 0:
-        search_result = lk.search_lightcurve(star, mission=mission)
-        if len(search_result) == 0:
-            return None
+        return None
 
-    filtered_result = filter_by_author(search_result)
+    missions = {'Kepler', 'TESS', 'K2'}
+
+    available_missions = {
+        mission for mission in missions if any(mission in result for result in search_result.mission)
+    }
+
+    if len(available_missions) > 1:
+
+        valid_choice = False
+
+        while valid_choice == False:
+
+            print('Light curve data from the following missions found: ' + ', '.join(available_missions))
+            mission_choice = input('Please select a mission by entering its name:  ').strip()
+
+            if mission_choice not in available_missions:
+                print('Invalid selection!')
+            else:
+                valid_choice = True
+        
+        # FIX THIS - only selects the first result
+        search_result = search_result[mission_choice in search_result.mission]
+        
+
+    # filtered_result = filter_by_author(search_result)
 
     print('Light curve data found!')
+    print(search_result)
 
-    lc = filtered_result[-1].download()
+    # Download most recent light curve to mitigate noise from thermal effects of launch
+    lc = search_result[-1].download()
 
     return lc
 
@@ -52,9 +78,9 @@ def clean_lc(light_curve):
     # Remove the NANs and outliers
     return light_curve.remove_nans().remove_outliers()
 
-def sonify_star(star_name, mission='TESS'):
+def sonify_star(star_name):
 
-    lc = search_for_star(star_name, mission)
+    lc = search_for_star(star_name)
 
     if lc is None:
         print('No light curve results found for ' + star_name)
