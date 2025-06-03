3389517fa42c09b2d927b8ae94f7d2482d98b1d4
Robin Nandi <robin.nandi@newcastle.ac.uk>
2025-06-03 11:11:09 +0100

    a few updates to the app

---
+++ app/app/components/Home.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "./List";
+
+export default function Home() {
+  
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
+  return (
+    <div className="main">
+      <h1>Light Curves</h1>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+      <List />
+    </div>
+  );
+
+}
---
+++ app/app/components/List.js
@@ -0,0 +1,27 @@
+import data from "./ListData.json"
+
+function List(props) {
+    
+    //create a new array by filtering the original array
+    const filteredData = data.filter((el) => {
+        //if no input the return the original
+        if (props.input === '') {
+            return el;
+        }
+        //return the item which contains the user input
+        else {
+            return el.text.toLowerCase().includes(props.input)
+        }
+    })
+
+    return (
+        <ul>
+            {data.map((item) => (
+                <li key={item.id}>{item.text}</li>
+            ))}
+        </ul>
+    )
+
+}
+
+export default List
