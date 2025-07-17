d06b4cbf590e1f738a0620e6887ce37243c18f74
Robin Nandi <robin.nandi@newcastle.ac.uk>
2025-07-17 17:42:35 +0100

    Added sonification component and routing

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { defaultSystem } from "@chakra-ui/react"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Lightcurves from './components/Lightcurves';
+import Sonification from './components/Sonification';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
     <ChakraProvider value={defaultSystem}>
-      <Lightcurves />  {/* new */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Lightcurves />} />
+          <Route path="/sonification" element={<Sonification />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   )
 }
