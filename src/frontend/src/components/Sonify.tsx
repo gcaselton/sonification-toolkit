cf045f75336e9bd66d2213e2c36828a35f983262
Robin Nandi <robin.nandi@newcastle.ac.uk>
2025-07-24 16:35:15 +0100

    calling select lightcurve API and added backend tmp folder to gitignore

--- src/frontend/src/components/Sonify.tsx
+++ src/frontend/src/components/Sonify.tsx
@@ -16,6 +16,11 @@ export default function Sonify() {
 
   const [length, setLength] = useState(10);
   const [audioSystem, setAudioSystem] = useState("stereo");
+  const location = useLocation();
+  const settingsFilepath = location.state.filepath;
+  const dataFilepath = location.state.dataFilepath;
+  console.log("Filepath of Selected Lightcurve:", dataFilepath);
+  console.log("Settings filepath:", settingsFilepath);
 
   const audioSystemOptions = createListCollection({
     items: [
@@ -31,12 +36,54 @@ export default function Sonify() {
     setAudioSystem(value);
   };
 
+  const requestSonification = async () => {
+    const url_sonification = "http://localhost:8000/sonify-lightcurve";
+    //data_filepath: str
+    //style_filepath: str
+    //duration: int
+    //system: str
+    const data = {
+      "data_filepath": dataFilepath,
+      "style_filepath": settingsFilepath,
+      "duration": length,
+      "system": audioSystem
+    };
+    const config = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    };
+    try {
+      const response = await fetch(url_sonification, config);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      console.log("Sonification result:", result);
+      return result.filename; // Assuming the result contains the sonification data
+    } catch (error) {
+      console.error("Error fetching sonification:", error);
+    }
+  }
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Log the length and audio system for debugging
     console.log("Sonification Length:", length);
     console.log("Audio System:", audioSystem);
-    // Submit the data to the server to get the sonification
+    requestSonification().then((filename) => {
+      if (filename) {
+        console.log("Sonification file created:", filename);
+        // You can handle the sonification file here, e.g., download it or play it
+      } else {
+        console.error("No sonification file returned.");
+      }
+    });
+
+
   };
 
   return (
