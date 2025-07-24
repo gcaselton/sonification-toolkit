f44cbbf9ada4e844f67bcb5bc9a70090aa4609c4
George Caselton <ngc133@newcastle.ac.uk>
2025-07-24 15:14:18 +0100

    Finished sonify light curve endpoint

--- src/backend/paths.py
+++ src/backend/paths.py
@@ -6,3 +6,7 @@ STYLE_FILES_DIR = SRC_DIR / "style_files"
 SOUND_ASSETS_DIR = SRC_DIR / "sound_assets"
 SYNTHS_DIR = SOUND_ASSETS_DIR / "synths"
 SAMPLES_DIR = SOUND_ASSETS_DIR / "samples"
+
+# Temp directory for storing data files and sonifications
+TMP_DIR = BACKEND_DIR / "tmp"
+TMP_DIR.mkdir(exist_ok=True)
