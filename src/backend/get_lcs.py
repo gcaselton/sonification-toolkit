fea2d4d269bb1b5a638a9f23e1a425c9add770ea
George Caselton <ngc133@newcastle.ac.uk>
2025-07-29 17:08:48 +0100

    Added small script for downloading example lightcurves

--- src/backend/get_lcs.py
+++ src/backend/get_lcs.py
@@ -1,12 +1,12 @@
 import lightkurve as lk
 import matplotlib.pyplot as plt
 
-# Search TESS Sector 58 data for a specific target (example: TIC ID or coordinates)
 search_result = lk.search_lightcurve(
-    target="beta persei",  # Replace this with a valid TIC ID or coordinates
-    mission="TESS",
-    author="SPOC",
-    sector=58
+    target="delta cephei"
+    # mission="TESS",
+    # author="SPOC",
+    # sector=58
 )
 
-search_result.download(download_dir='.')
+print(search_result)
+# search_result.download(download_dir='.')
