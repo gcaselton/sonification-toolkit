b7c67e0d47ddb31faf2d7f1c02a04972ae002030
George Caselton <ngc133@newcastle.ac.uk>
2025-07-29 14:32:56 +0100

    Fixed absolute filepaths for suggested styles

--- src/backend/paths.py
+++ src/backend/paths.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 BACKEND_DIR = Path(__file__).resolve().parent
 SRC_DIR = BACKEND_DIR.parent
-STYLE_FILES_DIR = SRC_DIR / "style_files"
+STYLE_FILES_DIR = BACKEND_DIR / "style_files"
 SOUND_ASSETS_DIR = SRC_DIR / "sound_assets"
 SYNTHS_DIR = SOUND_ASSETS_DIR / "synths"
 SAMPLES_DIR = SOUND_ASSETS_DIR / "samples"
