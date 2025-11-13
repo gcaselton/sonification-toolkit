5def78239681b70cc15c390cc91663092d6ebfb6
George Caselton <ngc133@newcastle.ac.uk>
2025-11-13 14:24:47 +0000

    fixed api url

--- src/frontend/src/apiConfig.ts
+++ src/frontend/src/apiConfig.ts
@@ -2,7 +2,7 @@ const viteEnv = import.meta.env.VITE_API_URL;
 const local = import.meta.env.VITE_SONIFICATION_BACKEND_URL
 const dev = 'http://127.0.0.1:8000'
 
-export const apiUrl = dev
+export const apiUrl = local
 export const coreAPI = `${apiUrl}/core`
 export const lightCurvesAPI = `${apiUrl}/light-curves`
 export const constellationsAPI = `${apiUrl}/constellations`
